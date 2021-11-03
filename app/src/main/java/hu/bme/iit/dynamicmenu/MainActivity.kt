ab0c7e6dd27849f176f72aa27a97cf7eae12e7b1
Anna Lauber <lauber.anna@gmail.com>
2021-11-03 18:23:38 +0100

    Modulba kiszervezve (piros)

--- app/src/main/java/hu/bme/iit/dynamicmenu/MainActivity.kt
+++ app/src/main/java/hu/bme/iit/dynamicmenu/MainActivity.kt
@@ -1,11 +1,8 @@
 package hu.bme.iit.dynamicmenu
 
-import android.annotation.SuppressLint
-import android.graphics.Color
 import android.os.Build
 import android.os.Bundle
 import android.view.Menu
-import android.view.MenuItem
 import androidx.annotation.RequiresApi
 import com.google.android.material.navigation.NavigationView
 import androidx.navigation.findNavController
@@ -15,9 +12,8 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.text.parseAsHtml
 import hu.bme.iit.dynamicmenu.databinding.ActivityMainBinding
-import java.util.*
+import hu.bme.iit.dynamicmenu.ui.dynamic.DynamicRedFunction
 import kotlin.collections.HashMap
 
 class MainActivity : AppCompatActivity() {
@@ -26,7 +22,7 @@ class MainActivity : AppCompatActivity() {
                                                          R.id.nav_gallery to 0,
                                                          R.id.nav_item to 0)
 
-    private var maxClickValue: Int = 0
+    private var function = DynamicRedFunction(clicksMap)
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
@@ -53,23 +49,25 @@ class MainActivity : AppCompatActivity() {
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
 
+        val navMenu: Menu = navView.menu
+
         // Kiválasztott menüelemre listener
         navView.setNavigationItemSelectedListener {
             menuItem ->
                 when(menuItem.itemId) {
                     R.id.nav_home -> {
-                        clicksMap.computeIfPresent(R.id.nav_home) { _, v -> v + 1 }
-                        changeNavMenuItemsColor()
+                        function.clicksMap.computeIfPresent(R.id.nav_home) { _, v -> v + 1 }
+                        function.changeMenuItemsColor(navMenu)
                         drawerLayout.closeDrawers()
                     }
                     R.id.nav_gallery -> {
-                        clicksMap.computeIfPresent(R.id.nav_gallery) { _, v -> v + 1 }
-                        changeNavMenuItemsColor()
+                        function.clicksMap.computeIfPresent(R.id.nav_gallery) { _, v -> v + 1 }
+                        function.changeMenuItemsColor(navMenu)
                         drawerLayout.closeDrawers()
                     }
                     R.id.nav_item -> {
-                        clicksMap.computeIfPresent(R.id.nav_item) { _, v -> v + 1 }
-                        changeNavMenuItemsColor()
+                        function.clicksMap.computeIfPresent(R.id.nav_item) { _, v -> v + 1 }
+                        function.changeMenuItemsColor(navMenu)
                         drawerLayout.closeDrawers()
                     }
                 }
@@ -86,36 +84,6 @@ class MainActivity : AppCompatActivity() {
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment_content_main)
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
-    }
-
-    // Megváltoztatja az adott nav menü elem title színét
-    fun MenuItem.setTitleColor(color: Int) {
-        val hexColor = Integer.toHexString(color).uppercase(Locale.getDefault()).substring(2)
-        val html = "<font color='#$hexColor'>$title</font>"
-        this.title = html.parseAsHtml()
-    }
-
-    // Kiszámolja minden navMenu elemre, hogy mennyire legyen piros.
-    // A függvény paraméterben megkapja a legtöbb kattintást, ez alapján számol.
-    @RequiresApi(Build.VERSION_CODES.O)
-    private fun calculateColors(maxClickValue: Int) {
-        val navView: NavigationView = binding.navView
-        val navMenu: Menu = navView.menu
-        clicksMap.forEach {
-                (key, value) ->
-            navMenu.findItem(key)
-                .setTitleColor(Color.valueOf((value.toFloat() / maxClickValue) * 1.0f,
-                                             0.0f,
-                                             0.0f).toArgb())
-        }
-    }
-
-    // Egymástól függően egyre pirosabbak a gyakran használt elemek (csúnya megoldás)
-    @SuppressLint("ResourceType")
-    @RequiresApi(Build.VERSION_CODES.O)
-    private fun changeNavMenuItemsColor() {
-        maxClickValue = clicksMap.maxByOrNull { it.value }?.value!!
-        calculateColors(maxClickValue)
     }
 
 }
